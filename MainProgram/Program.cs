37c469e3c068333bbc7dc5af4395926432e208d6
NikitaP <unntiss@bk.ru>
2022-09-13 17:15:54 +0300

    Add algorithm

--- MainProgram/Program.cs
+++ MainProgram/Program.cs
@@ -6,3 +6,36 @@ Console.WriteLine("Enter 2nd phrase: ");
 StringArray[1] = Console.ReadLine();
 Console.WriteLine("Enter 3rd phrase: ");
 StringArray[2] = Console.ReadLine();
+
+StrArray(StringArray);
+
+
+void StrArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            Console.Write($"{array[i]} ");
+        }
+    }
+}
+
+// int count(string[] arr)
+// {
+//     int sum = 0;
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         sum++;
+//     }
+//     return sum;
+// }
+
+// void ShowArray(int[] array)
+// {
+// for (int i = 0; i < array.Length; i++)
+// {
+// Console.Write(array[i] + " ");
+// }
+// Console.WriteLine();
+// }
