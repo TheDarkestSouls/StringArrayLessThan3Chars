9dfe3284173c7c2f47448cdb32898e4418d879ba
NikitaP <unntiss@bk.ru>
2022-09-13 16:37:36 +0300

    add input

--- MainProgram/Program.cs
+++ MainProgram/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] StringArray = new string[3];
+
+Console.WriteLine("Enter 1st phrase: ");
+StringArray[0] = Console.ReadLine();
+Console.WriteLine("Enter 2nd phrase: ");
+StringArray[1] = Console.ReadLine();
+Console.WriteLine("Enter 3rd phrase: ");
+StringArray[2] = Console.ReadLine();
